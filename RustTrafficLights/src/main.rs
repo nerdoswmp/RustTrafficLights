d704975a3488cd0cd8d27b23952e6342187ae479
nerdoswmp <nerdoswampo@gmail.com>
2025-10-19 20:01:24 -0300

    ✨ Initial commit
    Added base project files

---
+++ RustTrafficLights/src/main.rs
@@ -0,0 +1,35 @@
+/* 	Projeto do Cruzamento Automático - versão s1
+
+	Simula carros em um cruzamento simples
+*/
+
+
+
+/* Geometria do cruzamento
+
+						largura V
+				margem V|    |
+						|    |
+						|    |margem H
+------------------------+----+--------
+	ViaH  > > > 	   	|    |	    largura H
+------------------------+----+--------
+	perímetro H			|    |
+						|    |
+						|    |
+						| ^  |perímetro V
+						| ^  |
+						| ^  |
+						|ViaV|
+						|    |
+
+
+*/
+
+
+
+
+fn main() {
+    println!("Inicio do programa");
+    println!("Fim da simulação");
+}
